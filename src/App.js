1703afd5aaf39c762680e4e3dc334bc8eb523208
willian <willian.lauber@jala-foundation.org>
2022-04-20 13:53:48 -0300

    feat: categories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import "./categories.styles.scss";
 function App() {
+  const categories = [
+    
+      {
+        "id": 1,
+        "title": "hats",
+        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      },
+      {
+        "id": 2,
+        "title": "jackets",
+        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      },
+      {
+        "id": 3,
+        "title": "sneakers",
+        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      },
+      {
+        "id": 4,
+        "title": "womens",
+        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      },
+      {
+        "id": 5,
+        "title": "mens",
+        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+      }
+    
+  ];
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container"> </div>
-        category
-      </div>
+      {categories.map((category) => (
+      
+      ))}
     </div>
   );
 }
