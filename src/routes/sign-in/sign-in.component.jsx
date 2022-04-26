1f27fca47c5a2a5edafdf9b644a969f0af434d94
willian <willian.lauber@jala-foundation.org>
2022-04-26 15:40:28 -0300

    feat: google authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,18 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(user);
+  };
   return (
     <div>
       <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>Sign In with Google</button>
     </div>
   );
 };
