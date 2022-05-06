0263d02d2b493696e30cf07f7227e9bb5634a351
willian <willian.lauber@jala-foundation.org>
2022-05-06 16:59:34 -0300

    style: sign-up form styles

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,16 @@
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,11 @@
 import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import FormInput from "../form-input/form-input.component";
+import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
   displayName: "",
@@ -6,51 +13,86 @@ const defaultFormFields = {
   password: "",
   confirmPassword: "",
 };
-
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(formFields);
+    const { email, password, confirmPassword } = formFields;
+    if (password.value !== confirmPassword.value) {
+      console.log("error");
+      alert("Inputs dont match");
+      return;
+    }
+    try {
+      console.log("reseting 2");
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log("reseting 1");
+      await createUserDocumentFromAuth(user, { displayName });
+      console.log("reseting");
+      resetFormFields();
+      document.getElementByClassName("sign-up-form").reset();
+    } catch (error) {
+      if (error.code == "auth/email-already-in-use") {
+        console.log(error.message);
+      }
+    }
+  };
+
   const handleChange = (event) => {
-    const { name, value } = event;
+    console.log(event);
+    const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
   };
   return (
-    <div className="sign-up-form">
+    <div className="sign-up-form sing-up-container">
+      <h2>Don't have an account?</h2>
       <h1>Sign Up</h1>
-      <form onSubmit={""}>
-        <label>Display Name</label>
-        <input
+      <form>
+        <FormInput
+          label="Display Name"
           type="text"
           onChange={handleChange}
           name="displayName"
           required
-        ></input>
+        ></FormInput>
 
-        <label>E-mail</label>
-        <input
+        <FormInput
+          label="E-mail"
           type="email"
           onChange={handleChange}
           name="email"
           required
-        ></input>
+        ></FormInput>
 
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           onChange={handleChange}
           name="password"
           required
-        ></input>
+        ></FormInput>
 
-        <label>Confirm Password</label>
-        <input
+        <FormInput
+          label="Confirm Password"
           type="password"
           onChange={handleChange}
           name="confirmPassword"
           required
-        ></input>
-        <button type="submit">Sign-up</button>
+        ></FormInput>
+        <button type="action" onClick={handleSubmit}>
+          Sign-up
+        </button>
       </form>
     </div>
   );
