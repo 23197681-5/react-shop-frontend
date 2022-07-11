2e5b5241a878df289383a58ec29ae2073d3c5c73
willian <willian.lauber@jala-foundation.org>
2022-07-11 15:06:24 -0400

    chore: split env variables, netlify does not support json

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -31,7 +31,15 @@ import SHOP_DATA from "../../shop-data";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = process.env.firebaseConfig;
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
 
 // Initialize Firebase
 
